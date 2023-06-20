c5fc7cc76551d7b5feb50e71138d90376a1ba265
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-20 22:52:08 +0000

    Fix `aws help` test

--- test/aws-cli-persistence/with_node.sh
+++ test/aws-cli-persistence/with_node.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # check that `aws --help` works
-check "help" bash -c "aws --help | grep 'usage'"
+check "help" bash -c "aws help | grep 'usage'"
 
 # check that `.aws` and `/dc/aws-cli` exist under the user (should be node)
 check "~/.aws existence" bash -c "ls -la ~ | grep '.aws'"
