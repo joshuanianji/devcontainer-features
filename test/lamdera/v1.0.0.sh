d83c47cbbcd6ff4b471d9f36cd33f8db88beaae0
Joshua Ji <joshua.ji@jiwhiz.com>
2023-08-01 17:31:00 +0000

    some more tests

---
+++ test/lamdera/v1.0.0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library
+source dev-container-features-test-lib
+
+# NOTE: this is an "auto-generated" test, which means it will be
+# executed against an auto-generated devcontainer.json that
+# includes the 'lamdera' Feature with no options.
+#
+# https://github.com/devcontainers/cli/blob/main/docs/features/test.md
+#
+
+# check that running the binary by itself works
+check "normal" bash -c "lamdera"
+
+# check version
+check "version" bash -c "lamdera --version"
+
+# Report result
+reportResults
