462e81bc328bafec3dccf98bcd3afbd151cffebb
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-01 22:40:59 +0000

    Fix string comparisons

--- test/mount-pnpm-store/with_node.sh
+++ test/mount-pnpm-store/with_node.sh
@@ -10,14 +10,12 @@ source dev-container-features-test-lib
 # user is `node` in dev container
 
 echo "User: $(whoami)"
-ls -al ~
-
-echo "PNPM Store Path: $PNPM_STORE_PATH"
+pnpm config list
 
 # check that `pnpm config get store-dir` equals /home/node/.pnpm-store
 pnpmConfig=$(pnpm config get store-dir)
 echo "pnpm config get store-dir: $pnpmConfig"
-check "config" bash -c test "$pnpmConfig" = "/home/node/.pnpm-store"
+check "config" bash -c test $pnpmConfig -eq "/home/node/.pnpm-store"
 
 # Report result
 reportResults
--- test/mount-pnpm-store/with_pnpm.sh
+++ test/mount-pnpm-store/with_pnpm.sh
@@ -8,14 +8,12 @@ set -e
 source dev-container-features-test-lib
 
 echo "User: $(whoami)"
-ls -al /home/vscode
-
-echo "PNPM Store Path: $PNPM_STORE_PATH"
+pnpm config list
 
 # check that `pnpm config get store-dir` returns /home/node/.pnpm-store
 pnpmConfig=$(pnpm config get store-dir)
 echo "pnpm config get store-dir: $pnpmConfig"
-check "config" bash -c test "$pnpmConfig" = "/home/vscode/.pnpm-store"
+check "config" bash -c test $pnpmConfig -eq "/home/vscode/.pnpm-store"
 
 # Report result
 reportResults
