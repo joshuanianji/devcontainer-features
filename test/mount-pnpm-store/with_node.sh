68d7a72b0387664217490afc649790f377e3bd3a
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-01 22:25:06 +0000

    Fix scenario for vscode-user and node-user

--- test/mount-pnpm-store/with_node.sh
+++ test/mount-pnpm-store/with_node.sh
@@ -5,6 +5,11 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+# user is `node` in dev container
+
+echo "User: $(whoami)"
+ls -al ~
+
 echo "PNPM Store Path: $PNPM_STORE_PATH"
 
 # check that `pnpm config get store-dir` equals /home/node/.pnpm-store
--- test/mount-pnpm-store/with_pnpm.sh
+++ test/mount-pnpm-store/with_pnpm.sh
@@ -2,15 +2,20 @@
 
 set -e
 
+# User is `vscode` in dev container
+
 # Optional: Import test library
 source dev-container-features-test-lib
+
+echo "User: $(whoami)"
+ls -al /home/vscode
 
 echo "PNPM Store Path: $PNPM_STORE_PATH"
 
 # check that `pnpm config get store-dir` returns /home/node/.pnpm-store
 pnpmConfig=$(pnpm config get store-dir)
 echo "pnpm config get store-dir: $pnpmConfig"
-check "config" bash -c test "$pnpmConfig" = "/home/node/.pnpm-store"
+check "config" bash -c test "$pnpmConfig" = "/home/vscode/.pnpm-store"
 
 # Report result
 reportResults
