770ca1b2aab241c813b19df3590a9907c213a57d
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-09 06:25:49 +0000

    fix pnpm store again

--- src/mount-pnpm-store/install.sh
+++ src/mount-pnpm-store/install.sh
@@ -36,9 +36,17 @@ tee "$POST_CREATE_SCRIPT_PATH" >/dev/null \
 
 set -e
 
+# set pnpm config (if it's installed)
+if type pnpm >/dev/null 2>&1; then
+    echo "Setting pnpm store location to $_REMOTE_USER_HOME/.pnpm-store"
+    pnpm config set store-dir ~/.pnpm-store --global
+else
+    echo "WARN: pnpm is not installed! Please ensure pnpm is installed and in your PATH."
+    echo "WARN: pnpm store location will not be set."
+fi
 pnpm config set store-dir ~/.pnpm-store --global
 
-# if the user is not root, chown /dc/aws-cli to the user
+# if the user is not root, chown /dc/mounted-pnpm-store to the user
 if [ "$(id -u)" != "0" ]; then
     echo "Running post-start.sh for user $USER"
     sudo chown -R "$USER:$USER" /dc/mounted-pnpm-store
