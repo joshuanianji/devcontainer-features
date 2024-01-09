0d8b5a5433fbd3c8c74e0bf140473cb33208e885
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-09 01:16:33 +0000

    fix aws cli root user stuff

--- src/aws-cli-persistence/install.sh
+++ src/aws-cli-persistence/install.sh
@@ -23,3 +23,23 @@ fi
 ln -s /dc/aws-cli "$_REMOTE_USER_HOME/.aws"
 # chown .aws folder
 chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "$_REMOTE_USER_HOME/.aws"
+
+# --- Generate a 'aws-cli-persistence-post-create.sh' script to be executed by the 'postCreateCommand' lifecycle hook
+# Looks like this is the best way to run a script in lifecycle hooks
+# Source: https://github.com/devcontainers/features/blob/562305d37b97d47331d96306ffc2a0a3cce55e64/src/git-lfs/install.sh#L190C1-L190C109
+POST_CREATE_SCRIPT_PATH="/usr/local/share/aws-cli-persistence-post-create.sh"
+
+tee "$POST_CREATE_SCRIPT_PATH" >/dev/null \
+  <<'EOF'
+#!/bin/sh
+
+set -e
+
+# if the user is not root, chown /dc/aws-cli to the user
+if [ "$(id -u)" != "0" ]; then
+    echo "Running post-start.sh for user $USER"
+    sudo chown -R "$USER:$USER" /dc/aws-cli
+fi
+EOF
+
+chmod 755 "$POST_CREATE_SCRIPT_PATH"
--- test/aws-cli-persistence/zsh_shell.sh
+++ test/aws-cli-persistence/zsh_shell.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-# Optional: Import test library
-source dev-container-features-test-lib
-
-# check that `aws --help` works
-check "help" bash -c "aws help | grep 'usage'"
-
-# check that `.aws` and `/dc/aws-cli` exist under the user (should be node)
-check "~/.aws existence" bash -c "ls -la ~ | grep '.aws'"
-check "/dc/aws-cli existence" bash -c "ls -la /dc | grep 'aws-cli'"
-
-# check that the folders are owned by the user
-# `stat -c "%U %G" ~/.aws` returns "$USER $GROUP", in this case "node node"
-# https://askubuntu.com/a/175060
-check "~/.aws owned by user" bash -c "test \"$(stat -c "%U %G" ~/.aws)\" = 'node node'"
-check "/dc/aws-cli owned by user" bash -c "test \"$(stat -c "%U %G" /dc/aws-cli)\" = 'node node'"
-
-# Report result
-reportResults
+# Run default test script (in same folder)
+# See: https://github.com/devcontainers/features/blob/562305d37b97d47331d96306ffc2a0a3cce55e64/test/azure-cli/install_extensions_bookworm.sh
+./default.sh
