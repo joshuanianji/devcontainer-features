2a7783906e93bdffafb62f727ce24d92c45c435f
Joshua Ji <joshua.ji@jiwhiz.com>
2024-03-06 02:51:27 +0000

    update onCreate script

--- src/edgedb-cli/oncreate.sh
+++ src/edgedb-cli/oncreate.sh
@@ -1,4 +1,8 @@
-# if the user is not root, chown /dc/aws-cli to the user
+#!/usr/bin/env bash
+
+set -e
+
+# if the user is not root, chown /dc/edgedb-cli to the user
 if [ "$(id -u)" != "0" ]; then
     echo "Running oncreate.sh for user $USER"
     sudo chown -R "$USER:$USER" /dc/edgedb-cli
