08ef3b6edd914a71743d968cf8ad55c56b71b345
Joshua Ji <joshua.ji@jiwhiz.com>
2024-03-06 06:39:40 +0000

    test that data dir/config dir match up

--- test/edgedb-cli/_default.sh
+++ test/edgedb-cli/_default.sh
@@ -11,6 +11,10 @@ source dev-container-features-test-lib
 # check that the command is available
 check "help" bash -c "edgedb --help | grep 'Usage'"
 
+# make sure edgedb directories match up with our expectations
+check "edgedb cli data dir" bash -c "test \"$(edgedb info --get data-dir)\" = $HOME/.local/share/edgedb/data/"
+check "edgedb cli config dir" bash -c "test \"$(edgedb info --get config-dir)\" = $HOME/.config/edgedb/"
+
 # check that data directories exist
 check "~/.local/share/edgedb exists" bash -c "ls -la ~/.local/share | grep 'edgedb'"
 check "/dc/edgedb-cli/data exists" bash -c "ls -la /dc/edgedb-cli | grep 'data'"
@@ -21,13 +25,14 @@ check "/dc/edgedb-cli exists" bash -c "ls -la /dc/edgedb-cli | grep 'config'"
 
 # check that the folders are owned by the user
 # https://askubuntu.com/a/175060
-echo "Checking ownership of ~/.local/share/edgedb/data and /dc/edgedb-cli/data (ensure it is owned by $USER)"
+# $USER is empty when running as root, so we use $(whoami)
+echo "Checking ownership of ~/.local/share/edgedb/data and /dc/edgedb-cli/data (ensure it is owned by $(whoami))"
 
-check "~/.local/share/edgedb owned by user" bash -c "test \"$(stat -c "%U" ~/.local/share/edgedb)\" = $USER"
-check "/dc/edgedb-cli/data owned by user" bash -c "test \"$(stat -c "%U" /dc/edgedb-cli/data)\" = $USER"
+check "~/.local/share/edgedb owned by user" bash -c "test \"$(stat -c "%U" ~/.local/share/edgedb)\" = $(whoami)"
+check "/dc/edgedb-cli/data owned by user" bash -c "test \"$(stat -c "%U" /dc/edgedb-cli/data)\" = $(whoami)"
 
-check "~/.config/edgedb owned by user" bash -c "test \"$(stat -c "%U" ~/.config/edgedb)\" = $USER"
-check "/dc/edgedb-cli/config owned by user" bash -c "test \"$(stat -c "%U" /dc/edgedb-cli/config)\" = $USER"
+check "~/.config/edgedb owned by user" bash -c "test \"$(stat -c "%U" ~/.config/edgedb)\" = $(whoami)"
+check "/dc/edgedb-cli/config owned by user" bash -c "test \"$(stat -c "%U" /dc/edgedb-cli/config)\" = $(whoami)"
 
 # Report result
 reportResults
