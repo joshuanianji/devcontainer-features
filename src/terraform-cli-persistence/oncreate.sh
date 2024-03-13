97b5769c9143b78b448776d63cb4e77f4fac4581
Joshua Ji <joshua.ji@jiwhiz.com>
2024-03-13 22:09:02 +0000

    [terraform] use `oncreate.sh`

---
+++ src/terraform-cli-persistence/oncreate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# if the user is not root, chown /dc/aws-cli to the user
+if [ "$(id -u)" != "0" ]; then
+    echo "Running post-start.sh for user $USER"
+    sudo chown -R "$USER:$USER" /dc/terraform-cli
+fi
